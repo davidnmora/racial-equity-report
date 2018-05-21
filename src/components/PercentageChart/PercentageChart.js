48da61decab4c741e3252de5b2c5bf57961e6731
David Mora <davidnmora@gmail.com>
2018-05-20 17:14:10 -0700

    Setup SCSS compiler

--- src/components/PercentageChart/PercentageChart.js
+++ src/components/PercentageChart/PercentageChart.js
@@ -4,10 +4,6 @@ import './percentagechart.css'
 const VIS_HEIGHT = 500;
 
 export default class PercentageChart extends Component {
-	constructor(props) {
-		super(props)
-	}
-	
 	render() {
 		const { data, title } = this.props
 		return (
@@ -20,34 +16,20 @@ export default class PercentageChart extends Component {
 }
 
 function DotDataVis({ data }) {
-	console.log(data)
 	return (
 		<div className="dot-data-vis" style={{height: `${VIS_HEIGHT}px`}}>
 			{Object.keys(data ? data : {}).map((race, i) => {
 				return (
-					<div key={i} className={`percent-data-point ${race}`} style={{bottom: `${getPointPosition(data[race])}px`}}>{race}</div>
+					<div key={i} className={`percent-data-point ${race}`} style={{bottom: `${getPointPosition(data[race])}px`}}>
+						<span className="percent-data-point-title">{race}</span>
+					</div>
 				)
 			})}
-			
+		
 		</div>
-	)
-	return (
-		<table>
-			<tbody>
-			{Object.keys(data ? data : {}).map((race, i) => {
-				return (
-					<tr key={i}>
-						<td>{race}</td>
-						<td>{data[race]}</td>
-					</tr>
-				)
-			})}
-			</tbody>
-		</table>
 	)
 }
 
 function getPointPosition(percentage) {
-	console.log((percentage / 100) * VIS_HEIGHT)
 	return (percentage / 100) * VIS_HEIGHT
 }
