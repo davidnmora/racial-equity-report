ee196fcc4f3a3cd0795e71886be9f9ae58c91aaf
David Mora <davidnmora@gmail.com>
2018-05-20 17:50:05 -0700

    Adjust styling, global matching colors by .race

--- src/components/LikelyhoodFocus/LikelyhoodFocus.js
+++ src/components/LikelyhoodFocus/LikelyhoodFocus.js
@@ -6,14 +6,14 @@ export default function LikelyhoodFocus({ compareRaceA, toRaceB, likelyhood, yea
 		<div className="info-sub-container likelyhood-focus-container">
 			<div className="likelyhood-inner-wrapper">
 				<div className="text">For 3rd-8th graders in the {year} school year,
-					<span className="bold text"> {compareRaceA} students </span>
+					<span className={`bold text ${compareRaceA}`}> {compareRaceA} students </span>
 					were
 				</div>
 				<div className="likelyhood-number">{likelyhood}x</div>
 				<h3 className="likely">more likely</h3>
 				<div className="text">
 					than
-					<span className="bold text"> {toRaceB} students </span>
+					<span className={`bold text ${toRaceB}`}> {toRaceB} students </span>
 					to score
 					<span className="bold text"> {"'Career and College Ready'"} </span>
 					on end-of-grade exams.
